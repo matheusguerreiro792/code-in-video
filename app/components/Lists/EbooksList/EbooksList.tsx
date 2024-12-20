35ef241880643336ffb3278c845d8f23cf060368
Matheus Guerreiro <matheus.guerreiro.792@gmail.com>
2024-12-20 18:15:01 -0300

    feat: adjustments in lists. '

--- app/components/Lists/EbooksList/EbooksList.tsx
+++ app/components/Lists/EbooksList/EbooksList.tsx
@@ -6,14 +6,17 @@ export default function EbooksList() {
   const { ebooks } = useGlobalContext();
 
   return (
-    <ul className="bg-gray-700 flex flex-col gap-4 p-4 w-full rounded">
-      {ebooks?.length > 0 ? (
-        ebooks.map((ebook) => <EbookItem key={ebook.id} ebook={ebook} />)
-      ) : (
-        <label className="text-lg text-red-600 font-bold text-center">
-          There is no Ebooks!
-        </label>
-      )}
-    </ul>
+    <div className="flex flex-col w-full gap-2">
+      <h3 className="text-xl font-bold text-white ml-4">Ebook List</h3>
+      <ul className="bg-gray-700 flex flex-col gap-4 p-4 w-full rounded">
+        {ebooks?.length > 0 ? (
+          ebooks.map((ebook) => <EbookItem key={ebook.id} ebook={ebook} />)
+        ) : (
+          <label className="text-lg text-red-600 font-bold text-center">
+            There is no Ebooks!
+          </label>
+        )}
+      </ul>
+    </div>
   );
 }
--- app/components/Lists/VideosList/VideosList.tsx
+++ app/components/Lists/VideosList/VideosList.tsx
@@ -6,14 +6,17 @@ export default function VideosList() {
   const { videos } = useGlobalContext();
 
   return (
-    <ul className="bg-gray-700 flex flex-col gap-4 p-4 w-full rounded">
-      {videos?.length > 0 ? (
-        videos.map((video) => <VideoItem key={video.id} video={video} />)
-      ) : (
-        <label className="text-lg text-red-600 font-bold text-center">
-          There is no Videos!
-        </label>
-      )}
-    </ul>
+    <div className="flex flex-col w-full gap-2">
+      <h3 className="text-xl font-bold text-white ml-4">Video List</h3>
+      <ul className="bg-gray-700 flex flex-col gap-4 p-4 w-full rounded">
+        {videos?.length > 0 ? (
+          videos.map((video) => <VideoItem key={video.id} video={video} />)
+        ) : (
+          <label className="text-lg text-red-600 font-bold text-center">
+            There is no Videos!
+          </label>
+        )}
+      </ul>
+    </div>
   );
 }
